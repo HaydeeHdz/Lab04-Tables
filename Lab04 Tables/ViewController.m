a8419df4100b2c8321008f99ea6e8aa25b047619
Haydee <haydee@haydees-imac.local>
2016-06-21 19:05:34 -0500

    Initial Commit

---
+++ Lab04 Tables/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Lab04 Tables
+//
+//  Created by Haydee on 6/21/16.
+//  Copyright © 2016 Haydee. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
